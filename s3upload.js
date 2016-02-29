c754f45b16b9d46a9da3878feb8adc7ea0720b0e
Jon Brennecke <jpbrennecke@gmail.com>
2016-02-29 15:39:57 -0800

    added support for maximum file size

--- s3upload.js
+++ s3upload.js
@@ -3,22 +3,19 @@
  * https://github.com/flyingsparx/NodeDirectUploader
  */
 
-var latinize = require('latinize'),
-    unorm = require('unorm');
-
 S3Upload.prototype.signingUrl = '/sign-s3';
 S3Upload.prototype.fileElement = null;
 S3Upload.prototype.files = null;
 
-S3Upload.prototype.onFinishS3Put = function(signResult, file) {
+S3Upload.prototype.onFinishS3Put = function(signResult) {
     return console.log('base.onFinishS3Put()', signResult.publicUrl);
 };
 
-S3Upload.prototype.onProgress = function(percent, status, file) {
+S3Upload.prototype.onProgress = function(percent, status) {
     return console.log('base.onProgress()', percent, status);
 };
 
-S3Upload.prototype.onError = function(status, file) {
+S3Upload.prototype.onError = function(status) {
     return console.log('base.onError()', status);
 };
 
@@ -36,11 +33,11 @@ function S3Upload(options) {
 }
 
 S3Upload.prototype.handleFileSelect = function(files) {
+    this.onProgress(0, 'Waiting');
     var result = [];
     for (var i=0; i < files.length; i++) {
-        var file = files[i];
-        this.onProgress(0, 'Waiting', file);
-        result.push(this.uploadFile(file));
+        var f = files[i];
+        result.push(this.uploadFile(f));
     }
     return result;
 };
@@ -62,9 +59,9 @@ S3Upload.prototype.createCORSRequest = function(method, url) {
 };
 
 S3Upload.prototype.executeOnSignedUrl = function(file, callback) {
-    var normalizedFileName = unorm.nfc(file.name.replace(/\s+/g, "_"));
-    var fileName = latinize(normalizedFileName);
-    var queryString = '?objectName=' + fileName + '&contentType=' + encodeURIComponent(file.type);
+    var xhr = new XMLHttpRequest();
+    var fileName = file.name.replace(/\s+/g, "_");
+    var queryString = '?objectName=' + fileName + '&contentType=' + file.type;
     if (this.signingUrlQueryParams) {
         var signingUrlQueryParams = this.signingUrlQueryParams;
         Object.keys(signingUrlQueryParams).forEach(function(key) {
@@ -72,8 +69,7 @@ S3Upload.prototype.executeOnSignedUrl = function(file, callback) {
             queryString += '&' + key + '=' + val;
         });
     }
-    var xhr = this.createCORSRequest('GET',
-        this.server + this.signingUrl + queryString);
+    xhr.open('GET', this.signingUrl + queryString, true);
     if (this.signingUrlHeaders) {
         var signingUrlHeaders = this.signingUrlHeaders;
         Object.keys(signingUrlHeaders).forEach(function(key) {
@@ -88,54 +84,40 @@ S3Upload.prototype.executeOnSignedUrl = function(file, callback) {
             try {
                 result = JSON.parse(xhr.responseText);
             } catch (error) {
-                this.onError('Invalid signing server response JSON: ' + xhr.responseText, file);
+                this.onError('Invalid signing server response JSON: ' + xhr.responseText);
                 return false;
             }
             return callback(result);
         } else if (xhr.readyState === 4 && xhr.status !== 200) {
-            return this.onError('Could not contact request signing server. Status = ' + xhr.status, file);
+            return this.onError('Could not contact request signing server. Status = ' + xhr.status);
         }
     }.bind(this);
     return xhr.send();
 };
 
 S3Upload.prototype.uploadToS3 = function(file, signResult) {
-    var xhr = this.createCORSRequest('PUT', signResult.signedUrl);
+    var xhr = this.createCORSRequest('POST', signResult.signedUrl);
     if (!xhr) {
-        this.onError('CORS not supported', file);
+        this.onError('CORS not supported');
     } else {
         xhr.onload = function() {
             if (xhr.status === 200) {
-                this.onProgress(100, 'Upload completed', file);
-                return this.onFinishS3Put(signResult, file);
+                this.onProgress(100, 'Upload completed.');
+                return this.onFinishS3Put(signResult);
             } else {
-                return this.onError('Upload error: ' + xhr.status, file);
+                return this.onError('Upload error: ' + xhr.status);
             }
         }.bind(this);
         xhr.onerror = function() {
-            return this.onError('XHR error', file);
+            return this.onError('XHR error.');
         }.bind(this);
         xhr.upload.onprogress = function(e) {
             var percentLoaded;
             if (e.lengthComputable) {
                 percentLoaded = Math.round((e.loaded / e.total) * 100);
-                return this.onProgress(percentLoaded, percentLoaded === 100 ? 'Finalizing' : 'Uploading', file);
+                return this.onProgress(percentLoaded, percentLoaded === 100 ? 'Finalizing' : 'Uploading');
             }
         }.bind(this);
-    }
-    xhr.setRequestHeader('Content-Type', file.type);
-    if (this.contentDisposition) {
-        var disposition = this.contentDisposition;
-        if (disposition === 'auto') {
-            if (file.type.substr(0, 6) === 'image/') {
-                disposition = 'inline';
-            } else {
-                disposition = 'attachment';
-            }
-        }
-        var normalizedFileName = unorm.nfc(file.name.replace(/\s+/g, "_"));
-        var fileName = latinize(normalizedFileName);
-        xhr.setRequestHeader('Content-Disposition', disposition + '; filename=' + fileName);
     }
     if (this.uploadRequestHeaders) {
         var uploadRequestHeaders = this.uploadRequestHeaders;
@@ -147,7 +129,16 @@ S3Upload.prototype.uploadToS3 = function(file, signResult) {
         xhr.setRequestHeader('x-amz-acl', 'public-read');
     }
     this.httprequest = xhr;
-    return xhr.send(file);
+    var fileName = file.name.replace(/\s+/g, "_");
+    var form = new FormData();
+    form.append('key', signResult.key);
+    form.append('acl', 'private');
+    form.append('AWSAccessKeyId', signResult.accessKey);
+    form.append('success_action_status', 200);
+    form.append('Policy', signResult.policy);
+    form.append('Signature', signResult.signature);
+    form.append('file', file, fileName);
+    return xhr.send(form);
 };
 
 S3Upload.prototype.uploadFile = function(file) {
